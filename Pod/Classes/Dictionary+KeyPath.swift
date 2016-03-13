d57c60a33cd5cf14f778761bbf106aca455516f3
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-03-13 21:00:50 +0000

    0.1.5 - added cool operators

--- Pod/Classes/Dictionary+KeyPath.swift
+++ Pod/Classes/Dictionary+KeyPath.swift
@@ -1,15 +1,27 @@
 //
-//  Dictionary+KeyPath.swift
-//  Pods
+// Dictionary+KeyPath.swift
+// Pods
 //
-//  Created by Gabriel Coman on 12/03/2016.
+// Created by Gabriel Coman on 12/03/2016.
 //
+// This is a private extension to the Dictionary type that adds a form of
+// really simple Key-Path-Coding, similar to what NSDictionary has.
+// It does so by casting to NSDictionary for "get" operations, which should
+// allow it to obtain all the NSDictionary power.
+// For "set" however it just tries to create the appropriate dictionary or
+// array structure
 //
 
+//
+//
 import UIKit
 import Dollar
 
-public extension Dictionary where Key: StringLiteralConvertible, Value: AnyObject {
+extension Dictionary where Key: StringLiteralConvertible, Value: AnyObject {
+    
+    //
+    // the "get" at keyPath function simply casts a Dictionary as a NSDictionary
+    // and lets Cocoa work its magic
     func get(keyPath: String) -> AnyObject? {
         if let dict = (self as? AnyObject) as? Dictionary<String, AnyObject> {
             return (dict as NSDictionary).valueForKeyPath(keyPath)
@@ -17,6 +29,11 @@ public extension Dictionary where Key: StringLiteralConvertible, Value: AnyObjec
         return nil
     }
     
+    //
+    // The "set" value at keyPath function tries to create the structure that's
+    // required of it by the keyPath parameter, as best it can.
+    // It's implied that the keypath is valid, and the checking is fair but minimal
+    // no correction is applied
     mutating func set(val: AnyObject, keyPath: String) {
         var keys = keyPath.componentsSeparatedByString(".")
         guard let first = keys.first as? Key else { print("Unable to use string as key on type: \(Key.self)"); return }
@@ -38,24 +55,27 @@ public extension Dictionary where Key: StringLiteralConvertible, Value: AnyObjec
         }
     }
     
-    
-    mutating func setInArray(val: AnyObject, inArray keyPathForArray: String, atKeyPath keyPathInsideArray: String) {
-        var initial = self.get(keyPathForArray) as! [Dictionary]
-        let values = val as! [AnyObject]
-        
-        if initial.count == 0 {
-            $.each(values) { (i, object) in
-                var dict: Dictionary = [:]
-                dict.set(object, keyPath: keyPathInsideArray)
-                initial.append(dict)
-            }
-        }
-        else {
-            $.each(initial) { (i, _) in
-                initial[i].set(val[i], keyPath: keyPathInsideArray)
-            }
-        }
-        
-        self.set(initial as! AnyObject, keyPath: keyPathForArray)
-    }
+    //
+    // *** WARN ***
+    // This is deprecated and not included for now, but
+    // might be included in a future release, alongside some other new functionality
+//    mutating func setInArray(val: AnyObject, inArray keyPathForArray: String, atKeyPath keyPathInsideArray: String) {
+//        var initial = self.get(keyPathForArray) as! [Dictionary]
+//        let values = val as! [AnyObject]
+//        
+//        if initial.count == 0 {
+//            $.each(values) { (i, object) in
+//                var dict: Dictionary = [:]
+//                dict.set(object, keyPath: keyPathInsideArray)
+//                initial.append(dict)
+//            }
+//        }
+//        else {
+//            $.each(initial) { (i, _) in
+//                initial[i].set(val[i], keyPath: keyPathInsideArray)
+//            }
+//        }
+//        
+//        self.set(initial as! AnyObject, keyPath: keyPathForArray)
+//    }
 }
--- Pod/Classes/KeyPathTransformer.swift
+++ Pod/Classes/KeyPathTransformer.swift
@@ -1,35 +1,196 @@
 //
-//  KeyPathTransformer.swift
-//  Pods
+// KeyPathTransformer.swift
+// Pods
 //
-//  Created by Gabriel Coman on 12/03/2016.
+// Created by Gabriel Coman on 12/03/2016.
+//
+// KeyPathTransformer is a library that defines a special set of operators to
+// ease the work of transforming from one Dictionary (or NSDictionary) structure
+// to another.
+//  
+// The goal is to be able to do, as quickly and securely as possible,
+// transforms of the following type:
+//  
+// Given an original dictionary:
+//
+//      var dict = ["name": "John Applebee", "age": 23]
+//  
+// transform it into:
+//
+//      var dict2 = ["employee_name": "John Appleebee", "employee_age": 23]
+//
+// or any combinations thereof.
 //
 //
 
+//
+// import libraries
+// here UIKit is important but Dollar is used to add shorthand code instead of 
+// "for-loops"
 import UIKit
 import Dollar
 
-// define new operator
-infix operator => { associativity left precedence 140 } // 1
-public func =><T>(left: T, right: T) -> (T, T) {
-    return (left, right)
-}
-public func =>(left: AnyObject?, right: String) -> (AnyObject?, String) {
+////////////////////////////////////////////////////////////////////////////////
+// The following operator describes a transform
+////////////////////////////////////////////////////////////////////////////////
+
+// 
+// The "Describe Transform" operator
+// At it's core it serves two main purposes:
+// 
+// 1. Describe how a transform should look, e.g.:
+//
+//      "name" => "details.employee_name"
+//
+// 2. Assign a callback for more complex processing, if the fields being transformed 
+// are arrays or other more complex structures e.g. :
+//
+//      "employee" => "recruits" ==> { (namesDictionary) -> (Dictionary) in
+//          "name" => "recruit_name"
+//          "age"  => "recruit_age"
+//          ...
+//      }
+//
+infix operator => { associativity left precedence 140 }
+
+//
+// This generic version is used for transforms between one dictionary field to
+// another; the left hand side and right hand side parameters all 
+// all generic <T> parameters;
+// In real life, they should be of String or NSString type
+// This operator returns a simple tuple of type (T, T)
+public func => <A> (left: A, right: A) -> (A, A) {
     return (left, right)
 }
 
-// define the KeyPath Transform class
+//
+// This generic version is used for transforms in which a new field is added to
+// the transformed dictionary, which did not exist in the original one
+// In this case it take two generic A and B parameters;
+// In real life A can be any value (Int, Float?, complex object) and
+// B should be a String or NSString type
+// This operator returns a simple tuple of type (A, B)
+public func => <A, B> (left: A, right: B) -> (A, B) {
+    return (left, right)
+}
+
+//
+// This is a more complex version of the "Describe Transform" operator, which acts
+// as a way of making it a "ternary operator"
+// Simply put, when you do something like:
+//
+//      "name" => "employee_name" => { (dictionary) -> (dictionary in .... }
+//
+// the left-type associativity means that "name" => "employee_name" get first
+// transformed into a tuple of ("name", "employee_name"), and then that
+// tuple becomes the left-hand side parameter to the operator below.
+// The right hand side parameter is of course a function that takes a dictionary
+// as parameter (callback parameter) and returns another dictionary
+//
+// The whole operator returns a tuple between the original tuple and the function parameter
+public func => <A>(left: (A, A), right: (Dictionary<String, AnyObject>) -> (Dictionary<String, AnyObject>) ) -> (rule: (A, A), callback: (Dictionary<String, AnyObject>) -> (Dictionary<String, AnyObject>) ) {
+    return (left, right)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// The following two operator actually execute the transform
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// The second operator defined is the "Execute Addition" operator.
+infix operator +> { associativity left precedence 100 }
+
+//
+// This operator takes a left-hand side operator of type Keypath transform,
+// and a right-hand side operator of type tupe (AnyObject?, String).
+//
+// Its main purpose is to call the KeyPathTransformer function add() to set a new
+// value in the dictionary that is to be transformed.
+//
+// Because of the nature of the left and right parameters and the precedence given
+// each opearator, the final operation will look something like:
+//
+//      var transform = KeyPathTransformer(myDictionary)
+//      transform +> "Jonh Wood" => "employee_name"
+//
+// which literally tells the transform to add a new field to the dictionary that's
+// going to be created / transformed, called "employee_name" and set its value
+// to "John Wood";
+public func +> (left: KeyPathTransformer, right: (AnyObject?, String) ) {
+    left.add(right)
+}
+
+//
+// Finally this is the "Execute Transform" operator
+infix operator <> { associativity left precedence 100 }
+
+// 
+// In the most simple case the "Execute Transform" operator takes a 
+// left-hand side parameter of KeyPathTransformer type and a
+// right-hand side parameter of tuple type
+// Which means you will be able to do a chaining such as this:
+//
+//      var transform = KeyPathTransformer(myDictionary)
+//      transform <> "name" => "employee_name"
+//
+// Basically the transforms checks to see if the original dictionary, myDictionary, 
+// has a field named "name" and translates its value to a new field, called "employee_name", 
+// in the destination dictionary
+public func <> (left: KeyPathTransformer, right: (String, String) ) {
+    left.apply(right)
+}
+
+//
+// This is the same thing as above, only this allows you to add as right-hand
+// side parameter a tuple consisiting of another tuple (describing the transform) and
+// a callback function, that takes a dictionary as parameter (accessible from the callback) 
+// and expects a new dictionary as return type, possibly from another transform.
+//
+// This is usefull when transforming arrays of elements, like this:
+//
+//      var transform = KeyPathTransform(myDictionary)
+//      transform <> "employees" => "recruits" => { (employee) -> (Dictionary) in 
+//          var employeeTransform = KeyPathTransform(employee)
+//
+//          employeeTransform <> "name" => "rec_name"
+//          employeeTransform <> "age" => "details.age"
+//
+//          return employeeTransform
+//      }
+//
+public func <> (left: KeyPathTransformer, right: (rule: (String, String), callback: (Dictionary<String, AnyObject>) -> (Dictionary<String, AnyObject>) ) ) {
+    left.apply(right.rule, callback: right.callback)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Define a new class called KeyPathTransformer, which will contain the 
+// background function needed to actually perform the transformation.
+// Users shouldn't use this class' function directly, but by using the operators
+// defined above
+////////////////////////////////////////////////////////////////////////////////
+
 public class KeyPathTransformer: NSObject {
     
-    // references to two dictionaries
+    //
+    // Maintain two dictionaries 
+    // dictToTransform is the original one, which does not get mutated in any way
+    // dictTransformed is the dictionary that's being mutated, one operation after the other
     private var dictToTransform: Dictionary<String, AnyObject> = [:]
     private var dictTransformed: Dictionary<String, AnyObject> = [:]
     
+    // 
+    // constructor takes one parameter, of type dictionary
     public init(_ dict: Dictionary<String, AnyObject>) {
         super.init()
         dictToTransform = dict
     }
     
+    //
+    // Behind the "+>" operator is the add function, which simply sets a 
+    // certain value at a certain keypath, both specified as the first and second
+    // parameters in a tuple
+    // Note that the "set()" function is defined in the aux file Dictionary+KeyPath.
+    // Without it setting values at key paths in Swift Dictionaries is a lot harder
     public func add(set: (AnyObject?, String)) {
         let value = set.0
         let keyPath = set.1
@@ -39,6 +200,13 @@ public class KeyPathTransformer: NSObject {
         }
     }
     
+    //
+    // This is the simple form of the apply() function, which is what powers the
+    // "<>" operator.
+    // This simply takes one value from the source "dictToTransform" and sets it
+    // at a different key in the destination "dictTransformed" dictionary.
+    // This operation is of course not executed if the source dictionary does not
+    // contain that key
     public func apply(rule: (String, String)) {
         let source = rule.0
         let destination = rule.1
@@ -48,7 +216,13 @@ public class KeyPathTransformer: NSObject {
         }
     }
     
-    public func applyArray(rule: (String, String), callback: (Dictionary<String, AnyObject>) -> (Dictionary<String, AnyObject>)) {
+    //
+    // This more complex form of the above function tries to apply the same
+    // rule as above, but instead of simply mapping one key in the source dictionary
+    // to a new key in the destination dictionary while maintaining the value, 
+    // this looks at the "callback" function to transform the destination value
+    // as well.
+    public func apply(rule: (String, String), callback: (Dictionary<String, AnyObject>) -> (Dictionary<String, AnyObject>)) {
         let source = rule.0
         let destination = rule.1
         var result: [Dictionary<String, AnyObject>] = []
@@ -65,6 +239,8 @@ public class KeyPathTransformer: NSObject {
         dictTransformed.set(result, keyPath: destination)
     }
     
+    // 
+    // finally, this function returns the destination "dictTransformed"
     public func transform () -> Dictionary<String, AnyObject> {
         return dictTransformed
     }
